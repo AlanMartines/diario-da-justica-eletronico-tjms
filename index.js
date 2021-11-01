b1bc9d6233c6f2d0ad5cf41f4306f0ae5d85da40
Alan Martines <alancpmartines@hotmail.com>
2021-10-31 23:47:53 -0400

    Inicial Update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let scrape = async () => {
     height: 768,
     deviceScaleFactor: 1,
   });
+
   let postData = {
     'dadosConsulta.dtInicio': '02/11/2020',
     'dadosConsulta.dtFim': '03/11/2021',
@@ -15,34 +16,11 @@ let scrape = async () => {
     'dadosConsulta.pesquisaLivre': '"Alex Aparecido Pereira Martines"'
   };
 
-  await page.setRequestInterception(true);
-  page.once('request', request => {
-    var data = {
-      'method': 'POST',
-      'postData': querystring.stringify(postData),
-      'headers': {
-        ...request.headers(),
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-    };
-
-    request.continue(data);
-
-    // Immediately disable setRequestInterception, or all other requests will hang
-    page.setRequestInterception(false);
-  });
-  //
   await page.goto('https://esaj.tjms.jus.br/cdje/consultaAvancada.do');
-  const result = await page.evaluate(() => {
-    /*
-    const books = [];
-    document.querySelectorAll('section > div > ol > li img');
-      .forEach((book) => books.push(book.getAttribute('alt')));
-    return books;
-		*/
+  await page.screenshot({
+    path: 'example.png'
   });
-  browser.close();
-  return result;
+  await browser.close();
 };
 //
 scrape().then((value) => {
